05a0d7b2aec85530e84ac00ecb7db97e723260ac
SATHVIK-CONNECT <sathvikpalivela0@gmail.com>
2025-03-06 23:14:05 +0530

    Updated

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ tab_titles = [
     "About",
 ]
 
-whitespace = 16
+whitespace = 16.5
 ## Fills and centers each tab label with em-spaces
 tabs = st.tabs([s.center(whitespace,"\u2001") for s in tab_titles])
 
