921efbaa7517ee5d263a26b5f98076329d3d502b
Sathvik <sathvikpalivela0@gmail.com>
2025-11-07 07:29:08 +0530

    Update GROQ_API_KEY in app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ import tempfile
 st.set_page_config(page_title="Spark AI", page_icon="⚡")
 
 # Set up Groq API Key
-os.environ['GROQ_API_KEY'] = 'meta-llama/llama-4-scout-17b-16e-instruct'
+os.environ['GROQ_API_KEY'] = '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'
 
 # Styling
 canvas = st.markdown("""
